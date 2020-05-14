cb305eed6d230f08f8897cafc8fd5ccb16e29f87
Jessé  Del Sole Guimarães <jessedelsoleguimaraes@MacBook-Air-de-Jesse.local>
2020-05-14 00:37:54 -0300

    four more countries added

--- backend/src/database/seeds/01_cards.js
+++ backend/src/database/seeds/01_cards.js
@@ -21,17 +21,22 @@ exports.seed = function(knex) {
         {name: 'Africa do sul', url: 'https://cdn.countryflags.com/thumbs/south-africa/flag-800.png',population: 54841000, area: 1221000, hdi:0.705,pop_density:45 , safety_index:22.51},
         {name: 'Islândia', url: 'https://cdn.countryflags.com/thumbs/iceland/flag-800.png',population: 339000, area: 103000, hdi:0.938,pop_density:3 , safety_index:76.64},
         {name: 'Irã', url: 'https://cdn.countryflags.com/thumbs/iran/flag-800.png',population:  82021000, area: 1628000, hdi:0.797,pop_density:50 , safety_index:50.75},
-
         {name: 'Peru', url: 'https://cdn.countryflags.com/thumbs/peru/flag-800.png',population:  31036000, area: 1285000, hdi:0.759,pop_density:24 , safety_index:31.85},
         {name: 'Colombia', url: 'https://cdn.countryflags.com/thumbs/colombia/flag-800.png',population:  47698000, area: 1141000, hdi:0.761,pop_density:42 , safety_index:45.21},
         {name: 'Jamaica', url: 'https://cdn.countryflags.com/thumbs/jamaica/flag-800.png',population:  2990000, area: 10000 , hdi:0.726,pop_density:272 , safety_index:33.96},
         {name: 'Tailândia', url: 'https://cdn.countryflags.com/thumbs/thailand/flag-800.png',population:  68414135, area: 513000 , hdi:0.765,pop_density:133 , safety_index:59.52},
-        
         {name: 'Cuba', url: 'https://cdn.countryflags.com/thumbs/cuba/flag-800.png',population:  11147000, area: 109000 , hdi:0.778,pop_density:101 , safety_index:72.55},
-        {name: 'Espanha', url: 'https://cdn.countryflags.com/thumbs/spain/flag-800.png',population:  48958000, area: 505992 , hdi:0.893,pop_density:97 , safety_index:68.04},
+        {name: 'Espanha', url: 'https://cdn.countryflags.com/thumbs/spain/flag-800.png',population: 48958000, area: 505992 , hdi:0.893,pop_density:97 , safety_index:68.04},
+        {name: 'Etiópia', url: 'https://cdn.countryflags.com/thumbs/ethiopia/flag-800.png',population: 105350000, area: 1104300 , hdi:0.470,pop_density:95 , safety_index:50.79},
+        {name: 'Nova Zelândia', url: 'https://cdn.countryflags.com/thumbs/new-zealand/flag-800.png',population:  4510000, area: 275000 , hdi:0.921,pop_density:16 , safety_index:59.07},
+        {name: 'Bulgaria', url: 'https://cdn.countryflags.com/thumbs/bulgaria/flag-800.png',population: 7101510, area: 110879, hdi:0.816,pop_density:64 , safety_index:61.50},
+        {name: 'Marrocos', url: 'https://cdn.countryflags.com/thumbs/morocco/flag-800.png',population: 33986000, area: 446000, hdi:0.676,pop_density:76 , safety_index:51.31},
+        {name: 'Australia', url: 'https://cdn.countryflags.com/thumbs/australia/flag-800.png',population: 23232000, area: 7692024, hdi:0.938,pop_density:3 , safety_index:58.64},
+        {name: 'Canada', url: 'https://cdn.countryflags.com/thumbs/canada/flag-800.png',population: 35623000, area: 9984670, hdi:0.922,pop_density:4 , safety_index:60.33}
 
-        {name: 'Etiópia', url: 'https://cdn.countryflags.com/thumbs/ethiopia/flag-800.png',population:  105350000, area: 1104300 , hdi:0.470,pop_density:95 , safety_index:50.79},
-        {name: 'Nova Zelândia', url: 'https://cdn.countryflags.com/thumbs/new-zealand/flag-800.png',population:  4510000, area: 275000 , hdi:0.921,pop_density:16 , safety_index:59.07}
+
+        
+
         
         
         

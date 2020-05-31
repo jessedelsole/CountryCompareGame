8eb5b837b42a800b0e7aad7eeb09ad00e0856c61
Jessé  Del Sole Guimarães <jessedelsoleguimaraes@MacBook-Air-de-Jesse.local>
2020-05-31 00:01:21 -0300

    added button with loading effect on login

--- mobile/src/pages/Login/index.js
+++ mobile/src/pages/Login/index.js
@@ -1,30 +1,43 @@
-import React, { useState } from 'react';
-import { StyleSheet, Text, Image, TouchableOpacity, TextInput, KeyboardAvoidingView } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { StyleSheet, Text, Image, TouchableOpacity, TextInput, KeyboardAvoidingView, View, Dimensions, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import Constants from 'expo-constants';
 import { Feather } from '@expo/vector-icons';
 import api from '../../services/api';
+import Btn from 'react-native-micro-animated-button';
 
 export default function Login() {
 
   const navigation = useNavigation();
   const [name, setName] = useState('');
   const [carregando, setCarregando] = useState(false);
+
+  const loginButtonRef = useRef(null);
+
   let gameId = 0;
   let opponentName = '';
 
+  useEffect(() => {
+
+    global.timeOutCount = 0;
+
+  }, []);
 
   function onBtnClick() {
 
+    if (name ==''){
+        Alert.alert('Erro','Por favor informe um nome!');
+        loginButtonRef.current.reset();
+        return;
+    }
+    
     postLookForOpponent();
-
   }
 
   function postLookForOpponent() {
 
     setCarregando(true);
     api.post('lookForOpponent', { player: name, gameId }).then(result => trataResultadoPost(result));
-
   }
 
   function trataResultadoPost(result) {
@@ -33,47 +46,65 @@ export default function Login() {
     opponentName = result.data.opponentName;
 
     if (result.data.return_status == 2) {
+      loginButtonRef.current.reset();
       setCarregando(false);
       navigation.navigate('Game', { name, gameId, opponentName });
     } else {
 
-      console.log('chamando setTimeOut');
-      setTimeout(() => postLookForOpponent(), 2000)
+      global.timeOutCount++;
+      if (global.timeOutCount >= 10) {
+
+        loginButtonRef.current.reset();
+        setCarregando(false);
+        Alert.alert('Sem jogares online','No momento não há jogadores online. Tente convidar algum amigo para jogar também.');
+        global.timeOutCount = 0;
+      } else {
+
+        console.log('chamando setTimeOut');
+        setTimeout(() => postLookForOpponent(), 2000);
+      }
     }
   }
 
   return (
     <KeyboardAvoidingView behavior='height' style={styles.container}>
-      
-      <Text style={{width:'100%', textAlign:'center', fontWeight: 'bold', fontSize:28, color: '#333D79'}}>Country Compare Game</Text>
-      <Image style={{width:'100%',height:120, resizeMode:'contain'}}  source={require('../../../assets/icon.png')}> 
 
+      <Text style={{ width: '100%', textAlign: 'center', fontWeight: 'bold', fontSize: 28, color: '#333D79' }}>Country Compare Game</Text>
+      <Image style={{ width: '100%', height: 120, resizeMode: 'contain' }} source={require('../../../assets/icon.png')}>
       </Image>
+      <Text style={{ color: '#333D79', fontSize: 24, fontWeight: 'bold' }}>Informe seu nome:</Text>
 
-      <TouchableOpacity onPress={() => gameId = 0}>
-        <Text style={{ color: '#333D79', fontSize: 24, fontWeight: 'bold' }}>Informe seu nome:</Text>
-      </TouchableOpacity>
-
-      <TextInput style={{}} 
+      <TextInput style={{}}
+       editable ={!carregando}
         style={{
-          width: '100%', height: 60, borderColor: '#707070', borderWidth:1 , backgroundColor: 'white',
-          fontSize: 22, color:'#707070', justifyContent:'flex-end'
+          width: '100%', height: 60, borderColor: '#707070', borderWidth: 1, backgroundColor: 'white',
+          fontSize: 22, color: '#707070', justifyContent: 'flex-end'
         }}
-        onChangeText={(text) => setName(text)}
-      >
+        onChangeText={(text) => setName(text)}>
       </TextInput>
 
-      {carregando ? <Text style={{color:'#333D79',fontStyle:'italic'}}>Aguarde, procurando um oponente...</Text> : null}
+      {carregando ? <Text style={{width:'100%', textAlign:'center',  color: '#333D79', fontStyle: 'italic' }}>Aguarde, procurando um oponente online...</Text> : null}
 
-      <TouchableOpacity style={{
-        backgroundColor: '#333D79', height: 60, width: '100%', flexDirection: 'row', borderRadius: 0,
-        justifyContent: 'center', alignItems: 'center'
-      }}
-        onPress={onBtnClick}>
 
-        <Feather name="play" size={16} color="white" style={{ flex: 1, paddingLeft: 30 }}></Feather>
-        <Text style={{ color: '#ffffff', fontSize: 18, flex: 9, textAlign: 'center', paddingRight: 60 }}>Jogar</Text>
-      </TouchableOpacity>
+
+      <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+        <Btn style={{ height: 50 }}
+          ref={loginButtonRef}
+          label="Jogar"
+          onPress={onBtnClick}
+          successIcon="check"
+          backgroundColor='#333D79'
+          color= 'white'
+          foregroundColor='#707070'
+          maxWidth={Math.round(Dimensions.get('window').width - 10)}
+          minWidth={50}
+          labelStyle={{ color: '#FAEBFF', fontWeight: 'bold' }}
+        
+        />
+      </View>
+
+
+
     </KeyboardAvoidingView>
 
   );
